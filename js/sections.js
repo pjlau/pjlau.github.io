57e0cbd91d4efd90c1982d5c7097ca324a36f1c5
Lau <78383142+pjlau@users.noreply.github.com>
2024-07-04 08:55:25 +0800

    Update sections.js

--- js/sections.js
+++ js/sections.js
@@ -645,7 +645,21 @@ var scrollVis = function () {
    * formats we need to visualize
    *
    */
-
+  
+  /**
+   * getBinary - maps raw data to
+   * array of data objects. There is
+   * one data object for each word in the speach
+   * data.
+   *
+   * This function converts some attributes into
+   * numbers and adds attributes used in the visualization
+   *
+   * @param rawData - data read in from file
+   */
+  function getBinary(rawData, i) {
+      var rawBinary = rawData.filter(function (d) { return d.income50k == i; });
+  }
   /**
    * getWords - maps raw data to
    * array of data objects. There is
